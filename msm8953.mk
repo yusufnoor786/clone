b27c9afd9f3c1ed63bce9a64dd1389901fcfbb96
Alex Thundrous <63528549+AlexThundrous@users.noreply.github.com>
2020-10-09 13:17:17 +0530

    [TEMP] Commented callinfo to get the build started

--- msm8953.mk
+++ msm8953.mk
@@ -395,5 +395,5 @@ PRODUCT_COPY_FILES += \
 # Wi-Fi Display
 PRODUCT_BOOT_JARS += \
  WfdCommon
-PRODUCT_PACKAGES +=
- vendor.qti.ims.callinfo@1.0::IService
+#PRODUCT_PACKAGES +=
+ #vendor.qti.ims.callinfo@1.0::IService
