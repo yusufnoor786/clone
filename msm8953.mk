4f0abc1f6422b63d5b05db6d95fe747b9675be1e
AlexThundrous <alexthundrous2104@gmail.com>
2020-10-09 09:01:27 +0530

    Removed Telephony-ext

--- msm8953.mk
+++ msm8953.mk
@@ -307,10 +307,10 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
+    
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+
+    
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
