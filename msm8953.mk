9b8dab1e7a6b0adf9b1be96824e22017323f25ce
AlexThundrous <alexthundrous2104@gmail.com>
2020-10-26 00:48:50 +0000

    Update health hals
    Signed-off-by: AlexThundrous <alexthundrous2104@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -177,7 +177,8 @@ PRODUCT_COPY_FILES += \
 
 # health
 PRODUCT PACKAGES += \
-	android.hardware.health@2.0-service
+  android.hardware.health@2.0-impl \
+  android.hardware.health@2.0-service
 # Healthd
 PRODUCT_PACKAGES += \
   chargeonlymode
